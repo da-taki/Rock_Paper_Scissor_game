f5319c002931a90a30e2cbad0ca51d3810169eba
da-taki <singhtaknoor@gmail.com>
2024-10-17 19:34:51 +0530

    Adding code for exception handling

--- main.py
+++ main.py
@@ -15,14 +15,21 @@ def change_score(won,score):
 #Starting a while loop so that the player can play the game n number of times
 game_start = 1
 while game_start == 1:
-    game_start = int(input("Do you want to play rock paper scissors(1=yes,0=no)?: "))
+    #Exception Handling
+    try:
+        game_start = int(input("Do you want to play rock paper scissors(1=yes,0=no)?: "))
+    except ValueError:
+        print("Enter a number dum dum!")
+        continue
+    #Checking if they wanna play or not
     if game_start == 0:
         break
+    #The enemies option selecting algorithm 
     enemy_options = ['Rock','Paper','Scissors']
     enemy_choice = random.choice(enemy_options)
-
+    #Checking the player choice
     player_choice = input("Choose now!: ")
-
+    #Going through every possible outcome
     if player_choice.lower() == "rock" and enemy_choice.lower() == "rock":
         print(f"Your enemy chose {enemy_choice}")
         print("Its a draw!")
@@ -56,5 +63,12 @@ while game_start == 1:
     if player_choice.lower() == "scissors" and enemy_choice.lower() == "scissors":
         print(f"Your enemy chose {enemy_choice}")
         print("Its a draw!")
+
+    #Exception Handling
+    if player_choice.lower() != "rock" and player_choice.lower() != "paper" and player_choice.lower() != "scissor":
+        print("Chose either Rock, Paper or Scissor dum dum!")
+        continue
+
+    #Displaying the players Score
     print(f"Your Score is: {score}")
     
