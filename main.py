d7132a8a8f8be8efad31096298727eed558a52a3
da-taki <singhtaknoor@gmail.com>
2024-10-17 18:58:48 +0530

    Adding the score function

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #Importing the random library 
 import random
 
+#Setting up a score function
+score = 0
+
+#Setting up functions to either increase or decrease score
+def change_score(won,score):
+    if won:
+        score +=1
+    else:
+        score -=1
+    return score
+
 #Starting a while loop so that the player can play the game n number of times
 game_start = 1
 while game_start == 1:
@@ -18,25 +29,32 @@ while game_start == 1:
     if player_choice.lower() == "rock" and enemy_choice.lower() == "paper":
         print(f"Your enemy chose {enemy_choice}")
         print("The enemy wins :(")
+        score = change_score(False,score)
     if player_choice.lower() == "rock" and enemy_choice.lower() == "scissors":
         print(f"Your enemy chose {enemy_choice}")
         print("You WIN!")
+        score = change_score(True,score)
     if player_choice.lower() == "paper" and enemy_choice.lower() == "rock":
         print(f"Your enemy chose {enemy_choice}")
         print("You WIN!")
+        score = change_score(True,score)
     if player_choice.lower() == "paper" and enemy_choice.lower() == "paper":
         print(f"Your enemy chose {enemy_choice}")
         print("Its a draw!")
     if player_choice.lower() == "paper" and enemy_choice.lower() == "scissors":
         print(f"Your enemy chose {enemy_choice}")
         print("You lose :(")
+        score = change_score(False,score)
     if player_choice.lower() == "scissors" and enemy_choice.lower() == "rock":
         print(f"Your enemy chose {enemy_choice}")
         print("You lose :(")
+        score = change_score(False,score)
     if player_choice.lower() == "scissors" and enemy_choice.lower() == "paper":
         print(f"Your enemy chose {enemy_choice}")
         print("You win!")
+        score = change_score(True,score)
     if player_choice.lower() == "scissors" and enemy_choice.lower() == "scissors":
         print(f"Your enemy chose {enemy_choice}")
         print("Its a draw!")
+    print(f"Your Score is: {score}")
     
